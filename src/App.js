947820ed5570f7cbd02cfad2688d838d6d55467c
Ashwin Jain <1ashwinjain@gmail.com>
2024-01-09 15:25:06 -0600

    added real app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { SectionList } from "react-native";
 
+const products = [
+  { name: "Lower Level", id: 1 },
+  { name: "Concourse Level", id: 2 },
+  { name: "Upper Level", id: 3 },
+];
+
+function ListItem() {
+  function handleListItemClick() {
+    console.log("clicked");
+  }
+
+  const listItems = products.map((product) => (
+    // create a list of product items with each item having a unique id and onclick event
+    <li key={product.id} onClick={handleListItemClick}>
+      {product.name}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+function Button() {
+  return <button> Click me</button>;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Button />;
 }
 
 export default App;
